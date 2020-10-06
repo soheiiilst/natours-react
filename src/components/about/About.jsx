556081237f50d6e94df14d8103f8858c4be3f66c
Soheil <s.tehrani1994@gmail.com>
2020-10-07 01:15:51 +0330

    added responsive images, colored selected text and custom scrollbar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import HeadingSecondary from '../heading-secondary/HeadingSecondary'
+import HeadingSecondary from '../heading-secondary/HeadingSecondary';
 
 function About() {
   return (
@@ -9,19 +9,69 @@ function About() {
 
       <div className='row'>
         <div className='col-1-of-2'>
-          <h3 className="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
-          <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorum magni odit, aliquam vitae similique quos sint, voluptatem neque cupiditate repellat natus! Possimus laborum ipsam et ullam neque praesentium sint.</p>
+          <h3 className='heading-tertiary u-margin-bottom-small'>
+            You're going to fall in love with nature
+          </h3>
+          <p className='paragraph'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            dolorum magni odit, aliquam vitae similique quos sint, voluptatem
+            neque cupiditate repellat natus! Possimus laborum ipsam et ullam
+            neque praesentium sint.
+          </p>
 
-          <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
-          <p className="paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quaerat incidunt labore quasi vero optio omnis ullam, dolore odit?</p>
+          <h3 className='heading-tertiary u-margin-bottom-small'>
+            Live adventures like you never have before
+          </h3>
+          <p className='paragraph'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+            quaerat incidunt labore quasi vero optio omnis ullam, dolore odit?
+          </p>
 
           <button className='btn-text'>Learn more &rarr;</button>
         </div>
         <div className='col-1-of-2'>
-          <div className="composition">
-            <img src="img/nat-1-large.jpg" alt="Nature 1" className="composition__photo composition__photo--p1"/>
-            <img src="img/nat-2-large.jpg" alt="Nature 2" className="composition__photo composition__photo--p2"/>
-            <img src="img/nat-3-large.jpg" alt="Nature 3" className="composition__photo composition__photo--p3"/>
+          <div className='composition'>
+            <img
+              srcSet='img/nat-1.jpg 300w, img/nat-1-large.jpg 1000w'
+              sizes='(max-width: 900px) 20vw,(max-width: 600px) 30vw, 300px'
+              src='img/nat-1-large.jpg'
+              alt='Nature 1'
+              className='composition__photo composition__photo--p1'
+            />
+
+            <img
+              srcSet='img/nat-2.jpg 300w, img/nat-2-large.jpg 1000w'
+              sizes='(max-width: 900px) 20vw,(max-width: 600px) 30vw, 300px'
+              src='img/nat-2-large.jpg'
+              alt='Nature 2'
+              className='composition__photo composition__photo--p2'
+            />
+
+            <img
+              srcSet='img/nat-3.jpg 300w, img/nat-3-large.jpg 1000w'
+              sizes='(max-width: 900px) 20vw,(max-width: 600px) 30vw, 300px'
+              src='img/nat-3-large.jpg'
+              alt='Nature 3'
+              className='composition__photo composition__photo--p3'
+            />
+            
+            {/* 
+            <img
+              src='img/nat-1-large.jpg'
+              alt='Nature 1'
+              className='composition__photo composition__photo--p1'
+            />
+            <img
+              src='img/nat-2-large.jpg'
+              alt='Nature 2'
+              className='composition__photo composition__photo--p2'
+            />
+            <img
+              src='img/nat-3-large.jpg'
+              alt='Nature 3'
+              className='composition__photo composition__photo--p3'
+            />*/}
+
           </div>
         </div>
       </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='footer'>
+
       <div className="footer__logo-box">
-        <img src="img/logo-green-2x.png" alt="Natours" className='footer__logo' />
+        <picture className='footer__logo'>
+          <source srcSet="img/logo-green-small-1x.png 1x, img/logo-green-small-2x.png 2x" 
+            media="(max-width: 37.5em)"/>
+
+          <img srcSet="img/logo-green-1x.png 1x, img/logo-green-2x.png 2x" alt="Natours" src="img/logo-green-2x.png" />
+        </picture>
       </div>
+
       <div className="row">
         <div className="col-1-of-2">
           <div className="footer__navigation">
